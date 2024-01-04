53d9ff3f315acfaa177f100f154788e9fa142f25
Ramya <ramyanivaskannan@gmail.com>
2024-01-04 16:28:42 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class School(models.Model):
+    Sname=models.CharField(max_length=100)
+    Sprincipal=models.CharField(max_length=100)
+    Slocation=models.CharField(max_length=100)
