2aa7949c563d2370ca338d8eea794c206e224bc2
Ramya <ramyanivaskannan@gmail.com>
2024-01-10 11:56:28 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+    
+def validation_for_letter(data):
+    if data.lower().startswith('b'):
+        raise forms.ValidationError('started with b')
+
+def validate_length(data):
+    if len(data)<3:
+        raise forms.ValidationError('len is < 3')        
+
+
+
 
 class SchoolForm(forms.Form):
-    Sname=forms.CharField()
+    Sname=forms.CharField(validators=[validation_for_letter,validate_length])
     Sprincipal=forms.CharField()
     Slocation=forms.CharField()
+    email=forms.EmailField()
+    renteremail=forms.EmailField()
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        re=self.cleaned_data['renteremail']
+        if e!=re:
+            raise forms.ValidationError('emails not matched')    
