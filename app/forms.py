8c2c494c4108b42aa7d38e6c5e093a61b5da2d17
Ramya <ramyanivaskannan@gmail.com>
2024-01-10 17:55:42 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
     
 def validation_for_letter(data):
     if data.lower().startswith('b'):
@@ -12,14 +13,21 @@ def validate_length(data):
 
 
 class SchoolForm(forms.Form):
-    Sname=forms.CharField(validators=[validation_for_letter,validate_length])
+    Sname=forms.CharField(validators=[validation_for_letter,validators.MinLengthValidator(4)])
     Sprincipal=forms.CharField()
     Slocation=forms.CharField()
     email=forms.EmailField()
     renteremail=forms.EmailField()
+    botcatcher=forms.CharField(required=False,widget=forms.HiddenInput)
 
     def clean(self):
         e=self.cleaned_data['email']
         re=self.cleaned_data['renteremail']
         if e!=re:
             raise forms.ValidationError('emails not matched')    
+
+# used for avoiding automated server providing source code 
+    # def clean_botcatcher(self):
+       # b=self.cleaned_data['botcat'] 
+       # if len(b)>0:
+          #  raise forms.ValidationError('bot') 
